e66a211b84060dac3f1176c5bd241215bb0dbf14
Joshua Gosse <jlgosse@gmail.com>
2012-10-13 13:32:02 -0700

    Commit empty project

---
+++ TCDoorBell/TCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TCAppDelegate.h
+//  TCDoorBell
+//
+//  Created by Joshua Gosse on 2012-10-13.
+//  Copyright (c) 2012 Shopify. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TCDoorBell/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TCDoorBell
+//
+//  Created by Joshua Gosse on 2012-10-13.
+//  Copyright (c) 2012 Shopify. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TCAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([TCAppDelegate class]));
+	}
+}
